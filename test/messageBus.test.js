40abdc53ed843599983f74288da16cbd26be99ff
Kane Baccigalupi <baccigalupi@gmail.com>
2023-03-04 12:12:42 -0800

    Adds light weight message bus

---
+++ test/messageBus.test.js
@@ -0,0 +1,43 @@
+import { describe, it, expect, } from '../devDeps.ts';
+import { createBus } from '../lib/messageBus.js';
+
+describe('MessageBus', () => {
+  it('adds a listener and calls it when event is triggered', () => {
+    const listenerCalls = [];
+    const bus = createBus();
+
+    bus.subscribe('click', (payload) => {
+      listenerCalls.push(payload);
+    });
+
+    bus.publish('click', 'first click');
+    bus.publish('click', 'second click');
+
+    expect(listenerCalls[0]).toEqual('first click');
+    expect(listenerCalls[1]).toEqual('second click');
+  });
+
+  it('calls multiple listeners for the same event name', () => {
+    const listenerCalls = [];
+    const bus = createBus();
+
+    bus.subscribe('click', (payload) => {
+      listenerCalls.push({ message: 'first callback', payload });
+    });
+
+    bus.subscribe('click', (payload) => {
+      listenerCalls.push({ message: 'second callback', payload });
+    });
+
+    bus.publish('click', 'click bate');
+
+    expect(listenerCalls[0]).toEqual({
+      message: 'first callback',
+      payload: 'click bate',
+    });
+    expect(listenerCalls[1]).toEqual({
+      message: 'second callback',
+      payload: 'click bate',
+    });
+  });
+});
--- test/rendering/rootTemplates.test.jsx
+++ test/rendering/rootTemplates.test.jsx
@@ -1,5 +1,5 @@
 import { describe, expect, it, spy, xit } from '../../devDeps.ts';
-import { createTestDom, domToString } from '../support/testDom.js';
+import { createTestDom } from '../support/testDom.js';
 
 import jsx from '../../lib/jsx.js';
 import { bind } from '../../lib/rendering/templates/bound.ts';
@@ -16,10 +16,32 @@ describe('root templates', () => {
     const document = createTestDom();
     const state = { currentUser: { name: 'Fred', age: 83 } };
     const publish = () => {};
-    const renderKit = { document, state, publish };
+    const subscribe = spy();
+    const renderKit = { document, state, publish, subscribe };
 
     render(<BoundTemplate greeting="Hello" />, '#app', renderKit);
 
     expect(document.getElementById('app').innerHTML).toEqual('<h1>Hello Fred!</h1>');
+  });
+
+  xit('re-renders a bound template', () => {
+    const Template = ({ greeting, userName }) => (
+      <h1>{greeting} {userName}!</h1>
+    );
+    const viewModel = (state) => ({ userName: state.currentUser.name });
+    const BoundTemplate = bind(Template, viewModel);
+
+    const document = createTestDom();
+    const state = { currentUser: { name: 'Fred', age: 83 } };
+    const publish = () => {};
+    const subscribe = spy();
+    const renderKit = { document, state, publish, subscribe };
+
+    render(<BoundTemplate greeting="Hello" />, '#app', renderKit);
+
+    const newState = { currentUser: { name: 'Janet', age: 83 } };
+    publish("new-state", newState);
+
+    expect(document.getElementById('app').innerHTML).toEqual('<h1>Hello Janet!</h1>');
   });
 });
