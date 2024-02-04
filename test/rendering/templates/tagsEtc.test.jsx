4a42b6dc48f67b89d97921fef4cecb60970e7b77
Kane Baccigalupi <baccigalupi@gmail.com>
2024-02-04 09:23:10 -0800

    Lint and version bump; not yet published

--- test/rendering/templates/tagsEtc.test.jsx
+++ test/rendering/templates/tagsEtc.test.jsx
@@ -183,7 +183,7 @@ describe('Rendering static jsx', () => {
   })
 
   // This is causing a segfault when creating the ns element. I think this is a
-  // bug in the dom implementation?? 
+  // bug in the dom implementation??
   test.skip('svg creates tags and attributes via the right mechanisms', () => {
     const document = createTestDom()
     const createElementNS = document.createElementNS
@@ -193,15 +193,15 @@ describe('Rendering static jsx', () => {
 
     const Circle = () => {
       return (
-        <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-          <circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red" />
-        </svg> 
+        <svg height='100' width='100' xmlns='http://www.w3.org/2000/svg'>
+          <circle r='45' cx='50' cy='50' stroke='green' stroke-width='3' fill='red' />
+        </svg>
       )
     }
 
     const template = <Circle />
-    const [ node ] = template.render(renderKit)
-    
+    const [node] = template.render(renderKit)
+
     expect(document.createElementNS).toHaveBeenCalledTimes(2)
     expect(domToString(node)).toContain(
       '<circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red">'
--- test/templates/tag.test.jsx
+++ test/templates/tag.test.jsx
@@ -33,9 +33,9 @@ describe('Tag templates', () => {
     test('passes svg flag to children', () => {
       const Circle = () => {
         return (
-          <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-            <circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red" />
-          </svg> 
+          <svg height='100' width='100' xmlns='http://www.w3.org/2000/svg'>
+            <circle r='45' cx='50' cy='50' stroke='green' stroke-width='3' fill='red' />
+          </svg>
         )
       }
       const template = <Circle />
