9fde8745e088316bd96b24f302a55fe9cb09b21e
Kane Baccigalupi <baccigalupi@gmail.com>
2024-08-18 12:01:59 -0700

    Ports over input value change instructions

---
+++ lib/rendering/change/nodes/input.ts
@@ -0,0 +1,20 @@
+import { ChangeInstructions, JaxsElement, JaxsInput } from '../../../types'
+import { changeValue } from '../instructions'
+
+const notInput = (element: JaxsElement) => element.tagName !== 'INPUT'
+const valueSame = (sourceElement: JaxsElement, targetElement: JaxsElement) =>
+  (sourceElement as JaxsInput).value === (targetElement as JaxsInput).value
+
+export const compileForInputValue = (
+  sourceElement: JaxsElement,
+  targetElement: JaxsElement,
+) => {
+  if (notInput(sourceElement) || valueSame(sourceElement, targetElement)) {
+    return [] as ChangeInstructions
+  }
+
+  const source = sourceElement as JaxsInput
+  const target = targetElement as JaxsInput
+
+  return [changeValue(source, target, { name: 'value', value: target.value })]
+}
---
+++ test/rendering/change/nodes/input.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from 'vitest'
+import { createTestDom } from '../../../support/test-dom'
+import { ChangeInstructionTypes } from '../../../../lib/types'
+import { compileForInputValue } from '../../../../lib/rendering/change/nodes/input'
+
+describe('compileForInputValue instructions', () => {
+  it('returns an empty instruction set if the value is not an input', () => {
+    const document = createTestDom()
+    const source = document.createElement('div')
+    source.value = 'not-an-input'
+    const target = document.createTextNode('div')
+    source.value = 'still-not-an-input'
+
+    expect(compileForInputValue(source, target)).toEqual([])
+  })
+
+  it('returns an empty instruction set if the input value has not changed', () => {
+    const document = createTestDom()
+    const source = document.createElement('input')
+    source.value = 'abc'
+    const target = document.createElement('input')
+    target.value = 'abc'
+
+    expect(compileForInputValue(source, target)).toEqual([])
+  })
+
+  it('returns the right instructions for a change of input value', () => {
+    const document = createTestDom()
+    const source = document.createElement('input')
+    source.value = 'abc'
+    const target = document.createElement('input')
+    target.value = 'def'
+
+    expect(compileForInputValue(source, target)).toEqual([
+      {
+        source,
+        target,
+        type: ChangeInstructionTypes.changeValue,
+        data: {
+          name: 'value',
+          value: 'def',
+        },
+      },
+    ])
+  })
+})
