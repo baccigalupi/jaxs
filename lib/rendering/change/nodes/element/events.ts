7ebfb8e5184dc2d78a83909d84e0e5609d687b98
Kane Baccigalupi <baccigalupi@gmail.com>
2024-08-19 08:32:46 -0700

    Ports over event related change instructions

---
+++ lib/rendering/change/nodes/element/events.ts
@@ -0,0 +1,48 @@
+import type { JaxsElement, ChangeInstructions } from '../../../../types'
+import { addEvent, removeEvent, updateEvent } from '../../instructions'
+
+export const compileForEvents = (source: JaxsElement, target: JaxsElement) => {
+  const instructions = [] as ChangeInstructions
+  const sourceEventMaps = source.eventMaps
+  const targetEventMaps = target.eventMaps
+  const sourceDomEvents = Object.keys(sourceEventMaps)
+  const targetDomEvents = Object.keys(targetEventMaps)
+
+  sourceDomEvents.forEach((domEvent) => {
+    const sourceEventMap = sourceEventMaps[domEvent]
+    const targetEventMap = targetEventMaps[domEvent]
+
+    if (!targetEventMap) {
+      instructions.push(
+        removeEvent(source, target, {
+          name: sourceEventMap.domEvent,
+          value: sourceEventMap.listener,
+        }),
+      )
+    } else if (targetEventMap.busEvent !== sourceEventMap.busEvent) {
+      instructions.push(
+        updateEvent(source, target, {
+          name: domEvent,
+          targetValue: targetEventMap.listener,
+          sourceValue: sourceEventMap.listener,
+        }),
+      )
+    } // else events the same
+  })
+
+  targetDomEvents.forEach((domEvent) => {
+    const sourceEventMap = sourceEventMaps[domEvent]
+    const targetEventMap = targetEventMaps[domEvent]
+
+    if (!sourceEventMap) {
+      instructions.push(
+        addEvent(source, target, {
+          name: targetEventMap.domEvent,
+          value: targetEventMap.listener,
+        }),
+      )
+    }
+  })
+
+  return instructions
+}
---
+++ test/rendering/change/nodes/element/events.test.ts
@@ -0,0 +1,133 @@
+import { describe, expect, it, vi } from 'vitest'
+import { createTestDom } from '../../../../support/test-dom'
+import { ChangeInstructionTypes, JaxsElement } from '../../../../../lib/types'
+import { compileForEvents } from '../../../../../lib/rendering/change/nodes/element/events'
+
+describe('compileForEvents instructions', () => {
+  it('is empty when neither source nor target have an event map', () => {
+    const dom = createTestDom()
+    const source = dom.createElement('button') as JaxsElement
+    source.eventMaps = {}
+    const target = dom.createElement('button') as JaxsElement
+    target.eventMaps = {}
+
+    const instructions = compileForEvents(source, target)
+
+    expect(instructions).toEqual([])
+  })
+
+  it('is empty when all the events are the same', () => {
+    const dom = createTestDom()
+    const source = dom.createElement('button') as JaxsElement
+    source.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-to-somewhere',
+        listener: vi.fn(),
+      },
+    }
+    const target = dom.createElement('button') as JaxsElement
+    target.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-to-somewhere',
+        listener: vi.fn(),
+      },
+    }
+
+    const instructions = compileForEvents(source, target)
+
+    expect(instructions).toEqual([])
+  })
+
+  it('creates remove instructions when an event is no longer in the target', () => {
+    const dom = createTestDom()
+    const source = dom.createElement('button') as JaxsElement
+    source.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-to-server',
+        listener: vi.fn(),
+      },
+    }
+    const target = dom.createElement('button') as JaxsElement
+    target.eventMaps = {}
+
+    const instructions = compileForEvents(source, target)
+
+    expect(instructions).toEqual([
+      {
+        source,
+        target,
+        type: ChangeInstructionTypes.removeEvent,
+        data: {
+          name: 'click',
+          value: source.eventMaps.click.listener,
+        },
+      },
+    ])
+  })
+
+  it('creates an add instruction when a different event has been added to the target', () => {
+    const dom = createTestDom()
+    const source = dom.createElement('button') as JaxsElement
+    source.eventMaps = {}
+    const target = dom.createElement('button') as JaxsElement
+    target.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-to-server',
+        listener: vi.fn(),
+      },
+    }
+
+    const instructions = compileForEvents(source, target)
+
+    expect(instructions).toEqual([
+      {
+        source,
+        target,
+        type: ChangeInstructionTypes.addEvent,
+        data: {
+          name: 'click',
+          value: target.eventMaps.click.listener,
+        },
+      },
+    ])
+  })
+
+  it('creates an update instruction when an event of a certain name has changed', () => {
+    const dom = createTestDom()
+    const source = dom.createElement('button') as JaxsElement
+    source.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-to-server',
+        listener: vi.fn(),
+      },
+    }
+    const target = dom.createElement('button') as JaxsElement
+    target.eventMaps = {
+      click: {
+        domEvent: 'click',
+        busEvent: 'save-locally',
+        listener: vi.fn(),
+      },
+    }
+
+    const instructions = compileForEvents(source, target)
+
+    expect(instructions).toEqual([
+      {
+        source,
+        target,
+        type: ChangeInstructionTypes.updateEvent,
+        data: {
+          name: 'click',
+          targetValue: target.eventMaps.click.listener,
+          sourceValue: source.eventMaps.click.listener,
+        },
+      },
+    ])
+  })
+})
