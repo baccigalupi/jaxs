227dd7f119d0d7ae4eee13967cc7507442e875fa
Kane Baccigalupi <baccigalupi@gmail.com>
2024-09-26 08:33:59 -0700

    Changes the SVG Cypress tests:
    * Makes the viewport small and the svg big for easy visibility
    * Puts one of the elements into a `a` tag
    
    Conclusion, the `a` tag does have to be an svg element or it won't
    render the contents correctly. So, the user hack where the namespace
    is required, is required!

--- cypress/apps/src/svg.jsx
+++ cypress/apps/src/svg.jsx
@@ -7,8 +7,8 @@ export const EyeIcon = () => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="icon icon-tabler icon-tabler-eye"
-      width="24"
-      height="24"
+      width="300"
+      height="300"
       viewBox="0 0 24 24"
       stroke-width="2"
       stroke="currentColor"
@@ -19,7 +19,9 @@ export const EyeIcon = () => {
       <g stroke="red">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-        <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+        <a href="/go-somewhere" xmlns='http://www.w3.org/2000/svg'>
+          <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+        </a>
       </g>
     </svg>
   )
--- cypress/e2e/render-svg.cy.js
+++ cypress/e2e/render-svg.cy.js
@@ -1,11 +1,12 @@
 describe('template spec', () => {
   beforeEach(() => {
+    cy.viewport(320, 480)
     cy.visit('/svg.html')
   })
 
   it('has the correct size because it is correctly setup as svg', () => {
-    cy.get('svg').invoke('outerWidth').should('be.eq', 24)
-    cy.get('svg').invoke('outerHeight').should('be.eq', 24)
-    cy.get('svg g').invoke('outerHeight').should('be.eq', 24)
+    cy.get('svg').invoke('outerWidth').should('be.eq', 300)
+    cy.get('svg').invoke('outerHeight').should('be.eq', 300)
+    cy.get('svg g').invoke('outerHeight').should('be.eq', 300)
   })
 })
