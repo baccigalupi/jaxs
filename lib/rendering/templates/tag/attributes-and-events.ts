da0035e15407091ff6f122add35bee270a35c5c1
Kane Baccigalupi <baccigalupi@gmail.com>
2025-12-24 12:51:04 -0800

    Fixes unnecessary import

--- lib/rendering/templates/tag/attributes-and-events.ts
+++ lib/rendering/templates/tag/attributes-and-events.ts
@@ -6,10 +6,7 @@ import type {
   JsxCollection,
   PropValue,
 } from '../../../types'
-import {
-  ensureJsxChildrenArray,
-  normalizeJsxChildren,
-} from '../children/normalize'
+import { ensureJsxChildrenArray } from '../children/normalize'
 
 export const separateAttrsAndEvents = <T>(
   props: Props<T>,
